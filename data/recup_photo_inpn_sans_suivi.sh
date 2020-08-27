eff4bcc9193b76462ede0365b9faec3e0706d5d8
Marie Filipe <filipemarie@live.fr>
2020-08-27 11:17:08 +0200

    Update recup_photo_inpn_sans_suivi.sh

--- data/recup_photo_inpn_sans_suivi.sh
+++ data/recup_photo_inpn_sans_suivi.sh
@@ -156,5 +156,7 @@ cd "/home/${MYUSER}/"
 #   update  taxonomie.t_medias set desc_media = '' where desc_media = 'null';
 #   update  taxonomie.t_medias set auteur = '' where auteur = 'null';
 #   update  taxonomie.t_medias set url = '' where url = 'null';
-
+#   update  taxonomie.t_medias set desc_media = '' where desc_media is null;
+#   update  taxonomie.t_medias set auteur = '' where auteur is null;
+#   update  taxonomie.t_medias set url = '' where url is null;
 
